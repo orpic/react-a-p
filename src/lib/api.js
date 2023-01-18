723d3ab658e29fa8ce9a0dc54236d2b4a61c63b5
shobhit-u <shobhitumrao5@gmail.com>
2023-01-18 23:33:06 +0530

    api.js complete

---
+++ src/lib/api.js
@@ -0,0 +1,16 @@
+const fetchUrl =
+  "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+
+export async function getProducts() {
+  const response = await fetch(fetchUrl);
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Could not fetch products.");
+  }
+
+  //   console.log(data);
+
+  return data;
+}
