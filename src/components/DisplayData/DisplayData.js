815e6a4b6da97d310327fdf2b6e34035b9ed4b77
shobhit-u <shobhitumrao5@gmail.com>
2023-01-19 17:38:27 +0530

    pagination complete

--- src/components/DisplayData/DisplayData.js
+++ src/components/DisplayData/DisplayData.js
@@ -2,8 +2,37 @@ import React from "react";
 // eslint-disable-next-line
 import classes from "./DisplayData.module.css";
 
-const DisplayData = () => {
-  return <div>DisplayData</div>;
+const DisplayData = ({ data }) => {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" />
+            </th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((eachItem) => (
+            <tr id={eachItem.id}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>{eachItem.name} </td>
+              <td>{eachItem.email} </td>
+              <td>{eachItem.role} </td>
+              <td>Actions</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default DisplayData;
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,8 +2,40 @@ import React from "react";
 // eslint-disable-next-line
 import classes from "./Pagination.module.css";
 
-const Pagination = () => {
-  return <div>Pagination</div>;
+const Pagination = ({ nPages, currentPage, setCurrentPage }) => {
+  const pageNumbers = [...Array(nPages + 1).keys()].slice(1);
+
+  const nextPage = () => {
+    if (currentPage !== nPages) setCurrentPage(currentPage + 1);
+  };
+  const prevPage = () => {
+    if (currentPage !== 1) setCurrentPage(currentPage - 1);
+  };
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a onClick={prevPage} href="#">
+              Previous
+            </a>
+          </li>
+          {pageNumbers.map((pgNumber) => (
+            <li key={pgNumber}>
+              <a onClick={() => setCurrentPage(pgNumber)} href="#no-link">
+                {pgNumber}
+              </a>
+            </li>
+          ))}
+          <li>
+            <a onClick={nextPage} href="#no-link">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Pagination;
