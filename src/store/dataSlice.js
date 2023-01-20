84033acef38cc8e26fafcbae7b295c5fde767b12
shobhit-u <shobhitumrao5@gmail.com>
2023-01-20 23:43:52 +0530

    redux implementation

--- src/store/dataSlice.js
+++ src/store/dataSlice.js
@@ -2,9 +2,15 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const data = createSlice({
   name: "data",
-  initialState: [],
+  initialState: {
+    data: [],
+  },
   reducers: {
-    editData(state, action) {},
+    addData() {},
+    editData(state, action) {
+      const newData = action.payload;
+      const exisitingData = state.data.find((item) => item.id === newData.id);
+    },
     deleteData(state, action) {},
   },
 });
